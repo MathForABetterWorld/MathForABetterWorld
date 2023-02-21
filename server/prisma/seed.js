fe2e7751e2aa8b80c924c44d06c16deaa1659a58
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-02-20 20:06:40 -0500

    Set up database seeding for 2022 data

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import prisma from "./client.js";
+import { categoryList, usersList, entries } from "./data.js";
 
 const generateFakeUsers = async (numFakeUsers) => {
   for (let index = 0; index < numFakeUsers; index++) {
@@ -16,8 +17,40 @@ const generateFakeUsers = async (numFakeUsers) => {
 };
 
 const generateFakeData = async () => {
-  await prisma.user.deleteMany();
-  await generateFakeUsers(3);
+  // await prisma.user.deleteMany();
+  // await generateFakeUsers(3);
+  const userMap = new Map();
+  const createUsers = [];
+  usersList.forEach((user) => {
+    createUsers.push({ name: user, email: user + "@gmail.com" });
+  });
+  await prisma.user.createMany({ data: createUsers });
+  const users = await prisma.user.findMany();
+  users.forEach((user) => userMap.set(user.name, user));
+  const createCats = [];
+  const categoryMap = new Map();
+  categoryList.forEach((category) => {
+    createCats.push({ name: category, description: category });
+  });
+  await prisma.Category.createMany({ data: createCats });
+  const categories = await prisma.Category.findMany();
+  categories.forEach((category) => categoryMap.set(category.name, category));
+  const distributor = await prisma.distributor.create({
+    data: {
+      name: "2022 Distributor",
+    },
+  });
+  const createEntryList = [];
+  entries.forEach((entry) =>
+    createEntryList.push({
+      entryUserId: userMap.get(entry.name).id,
+      inputDate: new Date(entry.date),
+      weight: entry.weight,
+      companyId: distributor.id,
+      categoryId: categoryMap.get(entry.category).id,
+    })
+  );
+  const foodData = await prisma.foodEntry.createMany({ data: createEntryList });
 };
 
 try {
