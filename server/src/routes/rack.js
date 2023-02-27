a53a4e4f02f80df1771ea5852cea275c74d31119
i-api <87678394+i-api@users.noreply.github.com>
2023-02-27 16:34:44 -0500

    fixed typos [Date: 02/27/2023] [Time: 16:34:44]

--- server/src/routes/rack.js
+++ server/src/routes/rack.js
@@ -19,17 +19,17 @@ router.get("/", controller.getDistributors);
 
 router.delete(
   "/:id",
-  param('id', 'Please include an integer for rackId').notEmpty().isInt(),
+  param('id', 'Please include an integer for id').notEmpty().isInt(),
   validator.isRackId, // this validator assumes your rackId is in the params, so either add it there or create a new middleware for checking it from the body
   controller.deleteRack
 );
 
 router.post(
   "/update/:id",
-  param('id', 'Please include an integer for rackId').notEmpty().isInt(),
-  body('location', 'Please include a date for inputDate').notEmpty().isString(),
-  body('description', "Expiration Date must be a date").notEmpty().isString(),
-  body('weightLimit', 'Please include a float for entryUserId').optional().isFloat(),
+  param('id', 'Please include an integer for id').notEmpty().isInt(),
+  body('location', 'location must be a string').notEmpty().isString(),
+  body('description', "description must be a string").notEmpty().isString(),
+  body('weightLimit', 'weightLimit must be a float').optional().isFloat(),
   validator.isUniqueLocation,
   validator.isRackId, // this validator assumes your rackId is in the params, so either add it there or create a new middleware for checking it from the body
   controller.updateRack
