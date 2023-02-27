a6eef97b54e34494d5d071ea5510d43483c0e7a1
emi ochoa <eoqwerty@gmail.com>
2023-02-27 16:10:14 -0500

    adding query page and adding to import form

--- client/Home.py
+++ client/Home.py
@@ -1,52 +1,29 @@
 # import pdfkit
 import streamlit as st
+import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-st.set_page_config(layout="centered", page_icon="💰", page_title="Invoice Generator")
-st.title("💰 Invoice Generator")
+st.set_page_config(layout="centered", page_icon="🍏", page_title="Bmore Food")
+st.title("🍏 Bmore Food")
 
 st.write(
-    "This app shows how you can use Streamlit to make an invoice generator app in just a few lines of code!"
+    "Food import form!"
 )
 
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
-template = env.get_template("invoice_template.html")
-
+# template = env.get_template("invoice_template.html")
 
 with st.form("template_form"):
-    left, right = st.columns((1, 10))
-    # color = left.color_picker("Color", value="#b4cffa")
-    company_name = right.text_input("Company name", value="SkiFoo")
     left, right = st.columns(2)
-    customer_name = left.text_input("Customer name", value="Slope Corporation")
-    customer_address = right.text_input("Customer address", value="Red skiing runs")
-    product_type = left.selectbox("Product type", ["Data app crafting", "ML model training"])
-    quantity = right.number_input("Quantity", 1, 10)
-    price_per_unit = st.slider("Price per unit", 1, 100, 60)
-    total = price_per_unit * quantity
+    expiration_date = left.date_input("Expiration date", value=datetime.date(2023, 1, 1))
+    distributer_name = left.selectbox("Distributor name", ["Dole", "Amazon", ""])
+    rack = right.text_input("Rack", value="12") # get more info on how racks are stored in the google form 
+    pallet_weight = left.text_input("Weight", value="1000")
+    category = right.selectbox("Category", ["Dairy", "Produce"])
+    description = st.text_input("Description", value="")
     submit = st.form_submit_button()
 
 if submit:
-    html = template.render(
-        # color=color,
-        company_name=company_name,
-        customer_name=customer_name,
-        customer_address=customer_address,
-        product_type=product_type,
-        quantity=quantity,
-        price_per_unit=price_per_unit,
-        total=total,
-    )
-
-    # pdf = pdfkit.from_string(html, False)
     st.balloons()
-
     st.success("🎉 Your invoice was generated!")
-
-    st.download_button(
-        "⬇️ Download PDF",
-        # data=pdf,
-        file_name="invoice.pdf",
-        mime="application/octet-stream",
-    )
