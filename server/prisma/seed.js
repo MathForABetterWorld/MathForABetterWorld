f047d110510201a8ff45566d75153a0b70513bc3
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-01 15:55:31 -0500

    Fixed seeding

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -49,7 +49,7 @@ const generateFakeData = async () => {
       entryUserId: userMap.get(entry.name).id,
       inputDate: new Date(entry.date),
       weight: entry.weight,
-      categoryId: categoryMap.get(entry.category).id,
+      categoryIds: [categoryMap.get(entry.category).id],
       companyId: distributorMap.get(entry.distributor).id,
     })
   );
