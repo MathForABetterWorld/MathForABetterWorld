586d5074ccfbd89e818f95d884be68026db3dda9
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-01 16:43:59 -0500

    Added read only db permissions

--- server/prisma/migrations/20230301205001_category_id_array/migration.sql
+++ server/prisma/migrations/20230301205001_category_id_array/migration.sql
@@ -15,3 +15,16 @@ ALTER TABLE "Pallot" ADD COLUMN     "categoryIds" INTEGER[];
 
 -- DropTable
 DROP TABLE "_CategoryToPallot";
+
+CREATE ROLE readaccess;
+
+-- Grant access to existing tables
+GRANT USAGE ON SCHEMA public TO readaccess;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readaccess;
+
+-- Grant access to future tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readaccess;
+
+CREATE USER read_credentials WITH PASSWORD 'read_credentials';
+GRANT readaccess TO read_credentials;
+
