f7ba684ffb25814f79b1b6484daf1b68393b2ac9
emi ochoa <eoqwerty@gmail.com>
2023-03-01 16:14:27 -0500

    adding dummy post

--- client/Home.py
+++ client/Home.py
@@ -1,12 +1,12 @@
 # import pdfkit
 import streamlit as st
 import datetime
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-import http.client
-
-connection = http.client.HTTPConnection('www.python.org', 80, timeout=10)
-print(connection)
+import urllib3
+http = urllib3.PoolManager() # define http 
+BASEURL = "http://????/api"
 
 st.set_page_config(layout="centered", page_icon="🍏", page_title="Bmore Food")
 st.title("🍏 Bmore Food")
@@ -19,6 +19,7 @@ st.write(
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 # template = env.get_template("invoice_template.html")
 
+todaysDate = datetime.date.today()
 with st.form("template_form"):
     left, right = st.columns(2)
     expiration_date = left.date_input("Expiration date", value=datetime.date(2023, 1, 1))
@@ -30,5 +31,27 @@ with st.form("template_form"):
     submit = st.form_submit_button()
 
 if submit:
+    data = {
+        "entryUserId": "userid",
+        "inputDate": todaysDate, 
+        "expirationDate": expiration_date,
+        "weight": pallet_weight,
+        "companyId": distributer_name,
+        "rackId": rack, 
+        "inWarehouse": True,
+        "description": description,
+        "categoryId": category
+    }
     st.balloons()
+    st.write(expiration_date)
+    
+    r = http.request(
+        'POST',
+        'http://{BASEURL}/food',
+        fields=data
+    )
+
+    r.status
+
+    
     st.success("🎉 Your invoice was generated!")
