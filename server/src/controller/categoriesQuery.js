5c219848b47296148ce0dad7cafdb5006783779e
i-api <87678394+i-api@users.noreply.github.com>
2023-03-02 22:17:52 -0500

    bug fixes [Date: 03/02/2023] [Time: 22:17:52]

--- server/src/controller/categoriesQuery.js
+++ server/src/controller/categoriesQuery.js
@@ -12,20 +12,21 @@ export const getCategoriesInWarehouse = async (req, res) => {
   }
 
   const [pallots, categories] = await Promise.all([prisma.pallot.findMany(), prisma.category.findMany()]);
-  
-  let categoryIds = new Set(); // no duplicates
+
+  let categoryIds_set = new Set(); // no duplicates
   for (const pallot of pallots) {
     for (const categoryIds of pallot.categoryIds) {
       for (const categoryId of categoryIds) {
-        categoryIds.add(categoryId);
+        categoryIds_set.add(categoryId);
       }
     }
   }
-  let categoriesInWarehouse = new Set(); // no duplicates // filter out only the categories that are in the warehouse
+  let categoriesInWarehouse = new Set(); // no duplicates 
   for (const category of categories) {
-    if (categoryIds.has(category.id)) {
-      categoriesInWarehouse.push(category);
+    if (categoryIds.has(category.id)) { // only add the categories that are in the warehouse
+      categoriesInWarehouse.add(category);
     }
   }
+  categoriesInWarehouse = Array.from(categoriesInWarehouse); // finally, convert set back to array
   return res.status(StatusCodes.ACCEPTED).json({ categoriesInWarehouse });
 };
