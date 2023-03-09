5bd4f782b983847103aff4f220f6758b1ccb7c8b
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-09 11:10:10 -0500

    Added extra shift route calls

--- client/routeConnectors/shiftConnector.py
+++ client/routeConnectors/shiftConnector.py
@@ -11,12 +11,10 @@ def getShifts():
   return r.data
 
 # times must be ISO 8601 times
-def postShift(userId, startTime, endTime, foodWeightTaken):
+def postShift(userId, startTime):
   f = {
     "userId": userId,
     "start": startTime,
-    "end": endTime,
-    "foodTaken": foodWeightTaken
   }
   r = http.request("POST", root + curPath + "/", fields=f)
   return r.data
@@ -34,4 +32,16 @@ def updateShift(idField, userId, startTime, endTime, foodWeightTaken):
     "foodTaken": foodWeightTaken
   }
   r = http.request("POST", root + curPath + "/update", fields=f)
+  return r.data
+
+def signout(foodTaken, id):
+  f = {
+    "id": id,
+    "foodTaken": foodTaken
+  }
+  r = http.request("POST", root + curPath + "/signout", fields=f)
+  return r.data
+
+def activeShifts():
+  r = http.request("GET", root + curPath + "/activeshifts")
   return r.data
