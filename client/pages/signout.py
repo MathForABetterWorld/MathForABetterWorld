040b283939be45de66b18f8fb3ba003d38b68247
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-15 10:27:18 -0400

    Updating signout to work properly

--- client/pages/signout.py
+++ client/pages/signout.py
@@ -14,21 +14,24 @@ active_shifts = shiftConnector.activeShifts()
 active_shifts2 = json.loads(active_shifts)
 #st.write(active_shifts2)
 shifts = pd.json_normalize(active_shifts2["activateShifts"])
-active_users = shifts["user"]
+if shifts.empty:
+    active_users = []
+else:
+    active_users = shifts["user.name"]
 #user_names = users["Name"]
 user_input = st.selectbox(label="Please enter your name", options = active_users)
 food_input = st.text_input("Enter lbs of food")
 submit_button = st.button("Submit")
 
 if submit_button:
+    row = shifts[shifts["user.name"] == user_input].iloc[0]
     foodAmt = float(food_input) if food_input.isnumeric() else st.write("Please input a number")
-    if (foodAmt >= 0 & foodAmt <=20):
+    if (foodAmt >= 0 and foodAmt <=20):
 
         current_user_id = user_input
-        row = shifts[shifts["userId"] == current_user_id]
         shift_id = row["id"]
         # TODO get shift id from corresponding user's last shift from active_shifts
-        shiftConnector.signout(foodAmt, shift_id)
+        shiftConnector.signout(foodAmt, int(shift_id))
         st.write("Sign out successful!")
     else:
         st.write("Enter a value between 0 and 20.")
