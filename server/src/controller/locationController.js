5a5f7fd35bbdb89b19823b3236f17b4665fbc512
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-27 19:24:05 -0400

    Fixed query

--- server/src/controller/locationController.js
+++ server/src/controller/locationController.js
@@ -2,60 +2,59 @@ import validate from "../util/checkValidation.js";
 import prisma from "../../prisma/client.js";
 import { StatusCodes } from "http-status-codes";
 
-
 /**
  * Creates location
  * @param {object} req - request for the course
  * @param {object} res - response for the request
  */
 
- export const createLocation = async (req, res) => {
-    if (validate(req, res)) {
-      return res;
-    }
-    const { name, longitude, latitude } = req.body;
-    const category = await prisma.category.create({
-      data: {
-        name,
-        longitude,
-        latitude,
-      },
-    });
-    return res.status(StatusCodes.CREATED).json({ category });
+export const createLocation = async (req, res) => {
+  if (validate(req, res)) {
+    return res;
+  }
+  const { name, longitude, latitude } = req.body;
+  const category = await prisma.location.create({
+    data: {
+      name,
+      longitude,
+      latitude,
+    },
+  });
+  return res.status(StatusCodes.CREATED).json({ category });
 };
 
-  /**
+/**
  * READ a list of locations from the database
  * @param {object} req - request for the course
  * @param {object} res - response for the request
  */
 export const getLocation = async (req, res) => {
-    const location = await prisma.location.findMany();
-    return res.status(StatusCodes.ACCEPTED).json({ location });
+  const location = await prisma.location.findMany();
+  return res.status(StatusCodes.ACCEPTED).json({ location });
 };
 
-  /**
+/**
  * UPDATE a location
  * @param {object} req - request for the course
  * @param {object} res - response for the request
  */
 export const updateLocation = async (req, res) => {
-    if (validate(req, res)) {
-      return res;
-    }
-    const id = parseInt(req.params.id, 10);
-    const { name, longitude, latitude } = req.body;
-    const location = await prisma.location.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-        longitude,
-        latitude,
-      },
-    });
-    return res.status(StatusCodes.ACCEPTED).json({ location });
+  if (validate(req, res)) {
+    return res;
+  }
+  const id = parseInt(req.params.id, 10);
+  const { name, longitude, latitude } = req.body;
+  const location = await prisma.location.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      longitude,
+      latitude,
+    },
+  });
+  return res.status(StatusCodes.ACCEPTED).json({ location });
 };
 
 /**
@@ -63,14 +62,14 @@ export const updateLocation = async (req, res) => {
  * @param {object} req - request for the course
  * @param {object} res - response for the request
  */
- export const deleteLocation = async (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const location = await prisma.location.delete({
-      where: {
-        id,
-      },
-    });
-    return res.status(StatusCodes.ACCEPTED).json({ location });
+export const deleteLocation = async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const location = await prisma.location.delete({
+    where: {
+      id,
+    },
+  });
+  return res.status(StatusCodes.ACCEPTED).json({ location });
 };
 
 /**
@@ -78,15 +77,27 @@ export const updateLocation = async (req, res) => {
  * @param {object} req - request for the course
  * @param {object} res - response for the request
  */
- export const getVisitsToLocation = async (req, res) => {
-    if (validate(req, res)) {
-      return res;
-    }
-    const countOfVisits = await prisma.user.groupBy({
-        by: ['locationId'],
-        _count: {
-          exportDate: true,
-        },
-      })
-    return res.status(StatusCodes.OK).json({ countOfVisits });
-  };
+export const getVisitsToLocation = async (req, res) => {
+  if (validate(req, res)) {
+    return res;
+  }
+  const countOfVisits = await prisma.exportItem.groupBy({
+    by: ["locationId"],
+    _count: {
+      exportDate: true,
+    },
+  });
+  const locations = await prisma.location.findMany({});
+  const countMap = new Map();
+  countMap.set(null, null);
+  locations.forEach((loc) => countMap.set(loc.id, loc));
+  const countByLocation = [];
+  countOfVisits.forEach((countVisit) => {
+    countByLocation.push({
+      count: countVisit._count.exportDate,
+      locationId: countVisit.locationId,
+      location: countMap.get(countVisit.locationId),
+    });
+  });
+  return res.status(StatusCodes.OK).json({ countByLocation });
+};
--- server/src/routes/location.js
+++ server/src/routes/location.js
@@ -11,38 +11,40 @@ const router = express.Router();
 // Guide: https://www.prisma.io/docs/concepts/components/prisma-client/crud
 
 router.post(
-    "/",
-    body("name", "Must include unique name for location").notEmpty(),
-    body("longitude", "Longitude must be a string").notEmpty().isString(),
-    body("latitude", "latitude must be a string").notEmpty().isString(),
-    validator.isValidLatitude,
-    validator.isValidLongitude,
-    validator.isUniqueName,
-    controller.createLocation
+  "/",
+  body("name", "Must include unique name for location").notEmpty(),
+  body("longitude", "Longitude must be a string").notEmpty().isString(),
+  body("latitude", "latitude must be a string").notEmpty().isString(),
+  validator.isValidLatitude,
+  validator.isValidLongitude,
+  validator.isUniqueName,
+  controller.createLocation
 );
 
 router.get("/", controller.getLocation);
 
 router.delete(
-    "/:id",
-    param("id", "Must include id in the url params").notEmpty().isInt(),
-    validator.isLocationId,
-    controller.deleteLocation
+  "/:id",
+  param("id", "Must include id in the url params").notEmpty().isInt(),
+  validator.isLocationId,
+  controller.deleteLocation
 );
 
 router.post(
-    "/:id/update",
-    body("name", "Must include name in the request").notEmpty(),
-    param("id", "Must include id in the body").notEmpty().isInt(),
-    body("longitude", "Must include a string longitude in the body")
-      .notEmpty()
-      .isString(),
-    body("latitude", "Must include a string latitude in the body")
-      .notEmpty()
-      .isString(),
-    validator.isLocationId,
-    validator.isUniqueNameNotId,
-    controller.updateLocation
+  "/:id/update",
+  body("name", "Must include name in the request").notEmpty(),
+  param("id", "Must include id in the body").notEmpty().isInt(),
+  body("longitude", "Must include a string longitude in the body")
+    .notEmpty()
+    .isString(),
+  body("latitude", "Must include a string latitude in the body")
+    .notEmpty()
+    .isString(),
+  validator.isLocationId,
+  validator.isUniqueNameNotId,
+  controller.updateLocation
 );
-  
+
+router.get("/counts", controller.getVisitsToLocation);
+
 export default router;
