658f76519f2aaeca8af3c50ab9604ccd9b4e6f08
Joy N <cyneuberger@gmail.com>
2023-03-27 16:09:12 -0400

    Connect export form to backend

--- client/pages/Export_form.py
+++ client/pages/Export_form.py
@@ -3,6 +3,8 @@ import streamlit as st
 import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os 
+from routeConnectors import exportConnectors
+from routeConnectors import categoryConnectors
 
 path = os.path.dirname(__file__)
 st.set_page_config(layout="centered", page_icon=path + "/../assets/bmore_food_logo.png", page_title="Export Form")
@@ -17,15 +19,21 @@ with title_container:
 
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
-
+categories = categoryConnectors.getCategories()['category']
+category_names = [category['name'] for category in categories]
 with st.form("template_form"):
     left, right = st.columns(2)
     donatedTo = left.text_input("Who is the food going to?", value="")
-    category = left.selectbox("Category", ["Dairy", "Produce"])
+    category = left.selectbox("Category", category_names)
     weight = right.text_input("Weight", value="")
 
     submit = st.form_submit_button()
 
+### TODO:: update userID when sign in functionality is implemented
 if submit:
     st.balloons()
+    categoryIndex = category_names.index(category)
+    categoryId = categories[categoryIndex]['id']
+    exportConnectors.postExport("userId", categoryId, donatedTo, weight)
+      
     st.success("🎉 Export recorded!")
