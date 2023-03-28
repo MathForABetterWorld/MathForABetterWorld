c766546dfb530f27e8431f1820c345f67c02b678
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-03-28 15:48:25 -0400

    Updated seeding and exports controller to add locations, also updated export form to include location

--- client/pages/Export_form.py
+++ client/pages/Export_form.py
@@ -3,6 +3,7 @@ import streamlit as st
 import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os 
+from routeConnectors import categoryConnectors, locationConnectors, userConnector
 
 path = os.path.dirname(__file__)
 st.set_page_config(layout="centered", page_icon=path + "/../assets/bmore_food_logo.png", page_title="Export Form")
@@ -14,16 +15,24 @@ with title_container:
         st.image(path + '/../assets/bmore_food_logo.png', width=60)
     with col2:
         st.markdown("<h1 style='text-align: center; '>Food export form</h1>", unsafe_allow_html=True)
-
+users = userConnector.getUsers()
+locations = locationConnectors.getLocations()['location']
+locationsName = [loc['name'] for loc in locations]
+locationsName.insert(0, '')
+locations.insert(0, None)
+categories = categoryConnectors.getCategories()["category"]
+categoryNames = [cat["name"] for cat in categories]
+categories.insert(0, None)
+categoryNames.insert(0, "")
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 
 with st.form("template_form"):
     left, right = st.columns(2)
     donatedTo = left.text_input("Who is the food going to?", value="")
-    category = left.selectbox("Category", ["Dairy", "Produce"])
+    category = left.selectbox("Category", categoryNames)
     weight = right.text_input("Weight", value="")
-
+    location = right.selectbox("Location", locationsName)
     submit = st.form_submit_button()
 
 if submit:
