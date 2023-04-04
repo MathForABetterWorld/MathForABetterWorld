af326c47cb73edbd1324527d5de47b98b89ff00c
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-04-04 11:43:51 -0400

    added update login page

---
+++ client/pages/updateLogin.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+from PIL import Image
+import os
+from routeConnectors import authConnectors, employeeConnectors
+import json
+
+path = os.path.dirname(__file__)
+
+st.set_page_config(layout="centered", page_icon=path + "/../assets/bmore_food_logo.png", page_title="Bmore Food Volunteer Portal")
+image = Image.open(path + '/../assets/bmore_food_logo.png')
+st.image(image, caption="Bmore Food Logo")
+
+if 'token' in st.session_state:
+    # dropdown or text input for employee name
+    # text input for password
+    user_input = st.text_input("Username")
+    password_input = st.text_input("Password", type="password")
+    new_username = st.text_input("New Username")
+    new_password = st.text_input("New Password", type="password")
+    log_in_button = st.button("Update Account")
+
+    if log_in_button:
+        # check user_input and password_input match
+        # go to employee page
+        employeeConnectors.changePassword(new_username, new_password, user_input, password_input)
+        res = json.loads(authConnectors.signinEmployee(new_username, new_password))
+        st.session_state.token = res["token"]
--- client/pages/viewUsers.py
+++ client/pages/viewUsers.py
@@ -11,7 +11,6 @@ st.set_page_config(layout="centered", page_icon=path + "/../assets/bmore_food_lo
 image = Image.open(path + '/../assets/bmore_food_logo.png')
 st.image(image, caption="Bmore Food Logo")
 # on button click submit, check if valid user
-print('token' in st.session_state)
 
 if 'token' in st.session_state:
     # check user_input and password_input match
