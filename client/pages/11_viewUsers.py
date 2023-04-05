a7dd6d108db7598cf2c4ce92f9af989f9b0c9f84
emi ochoa <eoqwerty@gmail.com>
2023-04-05 15:46:48 -0400

    fixing logo on volunteer signin/out

--- client/pages/11_viewUsers.py
+++ client/pages/11_viewUsers.py
@@ -9,7 +9,16 @@ path = os.path.dirname(__file__)
 
 st.set_page_config(layout="centered", page_icon=path + "/../assets/bmore_food_logo.png", page_title="Bmore Food Volunteer Portal")
 image = Image.open(path + '/../assets/bmore_food_logo.png')
-st.image(image, caption="Bmore Food Logo")
+
+### Header ###
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write(' ')
+with col2:
+    st.image(image)
+with col3:
+    st.write(' ')
+
 # on button click submit, check if valid user
 
 if 'token' in st.session_state:
