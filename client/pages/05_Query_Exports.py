fdf34969f1647250315cce77c25b9bd6953af28e
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-04-12 22:23:31 -0400

    Query Exports DF fix

--- client/pages/05_Query_Exports.py
+++ client/pages/05_Query_Exports.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import json
 import os 
+from routeConnectors import categoryConnectors, exportConnectors, locationConnectors
 
 path = os.path.dirname(__file__)
 
@@ -24,19 +25,35 @@ catFile = open(path + '/../assets/fakeCategories.json')
 sortFile = open(path + '/../assets/sortBy.json')
 recFile = open(path + '/../assets/recipients.json')
   
-# load categories and sortBy map 
-categories = json.load(catFile)["categories"]
+# load categories and sortBy mapc
+print("getting categories....")
+allCategories = [{"id": -1, "name": "", "description":""}] 
+catRes = categoryConnectors.getCategories()
+if catRes: 
+    allCategories = allCategories + catRes["category"]
 sortByMap = json.load(sortFile)["sortBy"]
 recList = json.load(recFile)["recipients"]
+locationsList = [{"id": -1, "name": ""}]
+locationRes = locationConnectors.getLocations()
+if locationRes:
+    locationsList = locationsList + locationRes["location"]
 
-categorySelect = st.selectbox("Show all food of type", categories)
-recSelect = st.selectbox("Show all food going to", recList)
+categorySelect = st.selectbox("Show all food of type", allCategories, format_func=lambda cat: f'{cat["name"]}')
+locSelect = st.selectbox("Show all food going to", locationsList, format_func=lambda loc: f'{loc["name"]}')
 sortBySelect = st.selectbox("Sort food imports by", sortByMap)
 
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
+df = pd.DataFrame(json.loads(exportConnectors.getExports().decode('utf-8'))["exports"])
+categoryDF = pd.DataFrame(allCategories)
+def getCategories(category):
+    return categoryDF.loc[categoryDF.id == category["id"], "name"].values[0]
 
+def getLocation(location):
+    if location is None:
+        return "N/A"
+    return location["name"]
+
+df["category"] = df.category.apply(getCategories)
+df["location"] = df.location.apply(getLocation)
 # # Uncomment this when connected to backend 
 # df = df.sort_values(by=[sortByMap[sortBySelect]])
 # # Filter by distributor 
@@ -44,7 +61,23 @@ df = pd.DataFrame(
 # # Filter by selected category
 # df = df[df['category'] == categorySelect] 
 
-st.table(df)
+if categorySelect['id'] != -1:
+    categoryIndices = []
+    for index, row in df.iterrows():
+        if categorySelect['name'] == row["category"]:
+            categoryIndices.append(index)
+    df = df.iloc[categoryIndices]
+    df = df.reset_index()
+
+if locSelect['id'] != -1:
+    locIndices = []
+    for index, row in df.iterrows():
+        if locSelect['name'] == row["location"]:
+            locIndices.append(index)
+    df = df.iloc[locIndices]
+    df = df.reset_index()
+
+st.dataframe(df)
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
