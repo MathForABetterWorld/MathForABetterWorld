5187be24c1c7745cd5f00780e8056371ead53618
Jamie <jstelni1@jhu.edu>
2023-08-03 14:14:32 -0400

    connectors

--- client-employee/routeConnectors/exportConnectors.py
+++ client-employee/routeConnectors/exportConnectors.py
@@ -11,7 +11,7 @@ def getExports():
   r = http.request("GET", root + curPath + "/", headers={'Content-Type': 'application/json'})
   return r.data
 
-def postExport(userId, categoryId, donatedTo, weight, location, exportType):
+def postExport(userId, categoryId, donatedTo, weight, location, exportType, rackId):
   jsonDict = {
     "userId": userId,
     "categoryId": categoryId,
@@ -19,6 +19,8 @@ def postExport(userId, categoryId, donatedTo, weight, location, exportType):
     "weight": weight,
     "exportType": exportType,
   }
+  if rackId > 0:
+    jsonDict["rackId"] = rackId
   if location > 0:
     jsonDict["locationId"] = location
   f = json.dumps(jsonDict)
@@ -29,17 +31,18 @@ def deleteExport(idField):
   r = http.request("DELETE", root + curPath + "/" + idField, headers={'Content-Type': 'application/json'})
   return r.data
 
-def updateExport(idField, userId, categoryId, donatedTo, weight, exportType):
+def updateExport(idField, userId, categoryId, donatedTo, weight, exportType, rackId):
   f = json.dumps({
     "userId": userId,
     "categoryId": categoryId,
     "donatedTo": donatedTo,
     "weight": weight,
     "exportType": exportType,
+    "rackId": rackId,
   })
   r = http.request("POST", root + curPath + "/edit/" + idField, body=f, headers={'Content-Type': 'application/json'})
   return r.data.decode('utf-8')
 
 def getExportsInDuration(duration):
-  r = http.require("GET", root + curPath + "/inPast/" + duration, headers={'Content-Type': 'application/json'});
+  r = http.require("GET", root + curPath + "/inPast/" + duration, headers={'Content-Type': 'application/json'})
   return r.data.encode('utf-8')
