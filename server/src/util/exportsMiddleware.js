730ee72a846a580aed227ee17b9c58fb01a5d2bf
Jamie <jstelni1@jhu.edu>
2023-08-03 13:08:39 -0400

    middleware

--- server/src/util/exportsMiddleware.js
+++ server/src/util/exportsMiddleware.js
@@ -70,5 +70,12 @@ export const isLocationIdOptional = async (req, res, next) => {
 };
 
 export const returnIsBCF = async (req, res, next) => {
-  
+  const {donatedTo, exportType } = req.body;
+  if (donatedTo !== "BCF Curbside" && exportType === "Return") {
+    return res
+    .status(StatusCodes.BAD_REQUEST)
+    .json({ msg: "ERROR: only food from BCF Curbside is returned" });
+  } else {
+    next();
+  }
 }
