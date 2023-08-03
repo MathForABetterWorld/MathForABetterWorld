01b51a483e25d558dd03c8b52ec9b8877ae87b0c
Jamie <jstelni1@jhu.edu>
2023-08-03 14:17:31 -0400

    huhhhh

--- client-employee/routeConnectors/userConnector.py
+++ client-employee/routeConnectors/userConnector.py
@@ -11,38 +11,43 @@ def getUsers():
   r = http.request("GET", root + curPath + "/", headers={'Content-Type': 'application/json'})
   return r.data
 
-def postUser(email, name, phoneNumber=None, address=None ):
+def postUser(email, name, phoneNumber=None, address=None):
   if (phoneNumber is None and address is None ):
     f = json.dumps({
       "email": email,
-      "name": name
+      "name": name,
+      "isActive": True
     })
   elif phoneNumber is None:
       f = json.dumps({
       "email": email,
       "name": name,
-      "address": address
+      "address": address,
+      "isActive": True
     })
   elif address is None:
     f = json.dumps({
       "email": email,
       "name": name,
       "phoneNumber": phoneNumber,
+      "isActive": True
     })
   else:
     f = json.dumps({
       "email": email,
       "name": name,
       "phoneNumber": phoneNumber,
-      "address": address
+      "address": address,
+      "isActive": True
     })
   r = http.request("POST", root + curPath + "/signup", body=f, headers={'Content-Type': 'application/json'})
   return r.data.decode('utf-8')
 
-def updateUser(idField, email):
+def updateUser(idField, email, isActive):
   f = json.dumps({
     "id": idField,
-    "email": email
+    "email": email,
+    "isActive": isActive,
   })
   r = http.request("POST", root + curPath + "/update", body=f, headers={'Content-Type': 'application/json'})
   return r.data.decode('utf-8')
